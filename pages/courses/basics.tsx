04d655903cabfa41ffb887134d52fc1940ac7821
baltabay321 <abdollabaltabay@gmail.com>
2026-02-25 14:47:07 +0500

    Create basics.tsx file with course content

---
+++ pages/courses/basics.tsx
@@ -0,0 +1,19 @@
+// Basics Course Content
+
+const BasicsCourse = () => {
+    return (
+        <div>
+            <h1>Basics Course</h1>
+            <p>Welcome to the Basics Course! Here you'll find introductory materials on various topics.</p>
+            <h2>Course Outline</h2>
+            <ul>
+                <li>Introduction to the basics</li>
+                <li>Core concepts</li>
+                <li>Hands-on examples</li>
+                <li>Further reading</li>
+            </ul>
+        </div>
+    );
+};
+
+export default BasicsCourse;
