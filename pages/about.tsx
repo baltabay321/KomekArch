351eb1cdee6686a12579edf5bb37683a188297b0
baltabay321 <abdollabaltabay@gmail.com>
2026-02-25 14:46:55 +0500

    Create about page in pages/about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our KomekArch project. Here, we aim to build...</p>
+    </div>
+  );
+};
+
+export default AboutPage;
