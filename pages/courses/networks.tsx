6a575a604337e319e541fa971466fc8f573b7827
baltabay321 <abdollabaltabay@gmail.com>
2026-02-25 14:46:49 +0500

    Create networks course content file

---
+++ pages/courses/networks.tsx
@@ -0,0 +1,24 @@
+# Networks Course Content
+
+## Course Overview
+This course covers the fundamentals of network technologies, protocols, and architectures.
+
+## Module 1: Introduction to Networks
+- Definition of a computer network
+- Types of networks (LAN, WAN, etc.)
+- Network topologies
+
+## Module 2: Networking Protocols
+- Overview of protocols (TCP/IP, UDP)
+- The OSI model explained
+- Common networking protocols and their uses
+
+## Module 3: Network Security
+- Importance of network security
+- Common threats (malware, phishing)
+- Security protocols
+
+## Module 4: Practical Networking
+- Setting up a simple network
+- Network troubleshooting techniques
+- Use of networking tools (ping, traceroute)
